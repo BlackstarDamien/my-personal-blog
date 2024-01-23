8b2a5de7a8a63e3e580ea2ff225f8a2f3dae2bdb
BlackstarDamien <damianzylka@gmail.com>
2024-01-23 20:37:59 +0100

    Implement __str__() method in Article model

--- blog/models.py
+++ blog/models.py
@@ -4,3 +4,11 @@ from django.db import models
 class Article(models.Model):
     title = models.CharField(max_length=120)
     content = models.TextField()
+
+    def __str__(self) -> str:
+        """Generates string representation of model.
+
+        Returns:
+            str: String representation of Article.
+        """
+        return self.title
