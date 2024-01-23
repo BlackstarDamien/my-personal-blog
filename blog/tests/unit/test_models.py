e1c5997355439d791e64be5c5ddfb68348aad587
BlackstarDamien <damianzylka@gmail.com>
2024-01-23 20:37:43 +0100

    Add unit test about __str__() method

--- blog/tests/unit/test_models.py
+++ blog/tests/unit/test_models.py
@@ -12,4 +12,11 @@ class TestArticle(TestCase):
         article = Article.objects.get(title=data["title"])
         self.assertEquals(article.title, data["title"])
         self.assertEquals(article.content, data["content"])
-        
+    
+    def test_should_be_represented_by_title(self):
+        data = {
+            "title": "Test Article",
+            "content": "Test Content"
+        }
+        article = Article(**data)
+        self.assertEquals(data["title"], str(article))
