5d1b8182d0629d837d86a91540845f79a7919783
BlackstarDamien <damianzylka@gmail.com>
2024-01-24 20:06:59 +0100

    Implement simple view for main page

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def main_page(request: HttpRequest) -> HttpResponse:
+    return render(request, 'index.html')
