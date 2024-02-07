0397554cbe1dabf7f137c3c1e0951ee1494813d0
BlackstarDamien <damianzylka@gmail.com>
2024-02-07 19:58:42 +0100

    Adjust test_properly_displays_article_page()

--- blog/tests/e2e/test_article_page.py
+++ blog/tests/e2e/test_article_page.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from blog.models import Article
 from django.test import LiveServerTestCase
 from django.template.defaultfilters import slugify
 from selenium import webdriver
@@ -15,7 +16,6 @@ class TestArticlePage(LiveServerTestCase):
 
         self.article = {
             "title": "Test Article",
-            "publish_date": datetime.now().strftime("%Y-%m-%d"),
             "content": "Test Content"
         }
 
@@ -29,9 +29,11 @@ class TestArticlePage(LiveServerTestCase):
         - Publish date
         - Content
         """
+        self.create_dummy_article(self.article)
+        
         self.given_a_main_page()
         self.when_click_link(self.article["title"])
-        self.then_i_can_see_article_page()
+        self.then_i_can_see_article_page(self.article["title"])
 
     def given_a_main_page(self):
         """Goes to blog main page.
@@ -46,9 +48,12 @@ class TestArticlePage(LiveServerTestCase):
         """
         self.browser.find_element(By.LINK_TEXT, link_text).click()
     
-    def then_i_can_see_article_page(self):
+    def then_i_can_see_article_page(self, page_name: str):
         """Checks if article is displayed properly.
         """
+        expected_url = f"{self.live_server_url}/{slugify(page_name)}"
+        self.assertEqual(expected_url, self.browser.current_url)
+
         title = self.browser.find_elements(By.CSS_SELECTOR, ".article-title")
         publish_date = title = self.browser.find_elements(By.CSS_SELECTOR, ".article-pub-date")
         content = self.browser.find_elements(By.CSS_SELECTOR, ".article-content")
@@ -58,6 +63,14 @@ class TestArticlePage(LiveServerTestCase):
             "publish_date": publish_date,
             "content": content
         }
+        expected_article = {
+            "title": self.article["title"],
+            "publish_date": datetime.now().strftime("%Y-%m-%d"),
+            "content": self.article["content"]
+        }
+        self.assertDictEqual(current_article, expected_article)
 
-        self.assertEqual(f"{self.live_server_url}/{slugify(title)}", self.browser.current_url)
-        self.assertDictEqual(self.article, current_article)
+    def create_dummy_article(self, article):
+        """Creates dummy article.
+        """
+        Article.objects.create(**article)
