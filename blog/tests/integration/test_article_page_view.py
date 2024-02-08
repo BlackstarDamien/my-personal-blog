4e0404db262330611dba01550919322e283bcd1e
BlackstarDamien <damianzylka@gmail.com>
2024-02-08 21:22:51 +0100

    Correct test case test_article_page_returns_correct_html()

--- blog/tests/integration/test_article_page_view.py
+++ blog/tests/integration/test_article_page_view.py
@@ -3,13 +3,20 @@ from django.test import TestCase
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 from blog.views import article_page
+from blog.models import Article
 
 
 class TestArticlePageView(TestCase):
+    def setUp(self) -> None:
+        self.article = Article.objects.create(
+            title="Test Article 1",
+            content="Test Content"
+        )
+    
     def test_article_page_returns_correct_html(self):
         """Test that main page returns proper html file.
         """
         request = HttpRequest()
-        reponse = article_page(request)
+        reponse = article_page(request, self.article.slug)
         expected_html = render_to_string('article.html')
         self.assertEqual(reponse.content.decode(), expected_html)
