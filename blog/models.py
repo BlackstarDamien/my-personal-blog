98dd0eaaff97d5e17318c71d67354d97bd67314a
BlackstarDamien <damianzylka@gmail.com>
2024-02-08 21:21:12 +0100

    Overwrite save() method for Article model

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 
 class Article(models.Model):
     title = models.CharField(max_length=120)
     content = models.TextField()
+    slug = models.SlugField(null=False)
 
     def __str__(self) -> str:
         """Generates string representation of model.
@@ -12,3 +14,8 @@ class Article(models.Model):
             str: String representation of Article.
         """
         return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug or self.slug == '':
+            self.slug = slugify(self.title)
+        return super().save(*args, **kwargs)
