85c6cdae1c284211fb285ca3e4c3c003b6eb1501
BlackstarDamien <damianzylka@gmail.com>
2024-02-08 20:44:30 +0100

    Implement article_page() view

--- blog/views.py
+++ blog/views.py
@@ -7,3 +7,6 @@ def main_page(request: HttpRequest) -> HttpResponse:
     articles = Article.objects.all()
     context = {"articles": articles}
     return render(request, 'index.html', context)
+
+def article_page(request: HttpRequest) -> HttpResponse:
+    return render(request, 'article.html')
