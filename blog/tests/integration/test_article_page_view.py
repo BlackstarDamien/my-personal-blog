6d356cb5bcbebb35c8f84d498e0898d5a45de420
BlackstarDamien <damianzylka@gmail.com>
2024-02-08 22:54:35 +0100

    Create unit test test_articles_page_receives_proper_context_data()

--- blog/tests/integration/test_article_page_view.py
+++ blog/tests/integration/test_article_page_view.py
@@ -20,3 +20,9 @@ class TestArticlePageView(TestCase):
         reponse = article_page(request, self.article.slug)
         expected_html = render_to_string('article.html')
         self.assertEqual(reponse.content.decode(), expected_html)
+
+    def test_articles_page_receives_proper_context_data(self):
+        """Tests that article page returns proper data in context.
+        """
+        response = self.client.get(self.article.get_absolute_url())
+        self.assertEqual(self.article, response.context["article"])
