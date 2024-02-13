efe8aa73b48e1f1d7859ef7888094ec218af6f26
BlackstarDamien <damianzylka@gmail.com>
2024-02-13 18:22:18 +0100

    Refactor integration tests

--- blog/tests/integration/test_article_page_view.py
+++ blog/tests/integration/test_article_page_view.py
@@ -8,15 +8,16 @@ class TestArticlePageView(TestCase):
             title="Test Article 1",
             content="Test Content"
         )
+        self.article_url = self.article.get_absolute_url()
     
     def test_article_page_returns_correct_html(self):
         """Test that main page returns proper html file.
         """
-        response = self.client.get(self.article.get_absolute_url())
+        response = self.client.get(self.article_url)
         self.assertTemplateUsed(response, 'article.html')
 
     def test_articles_page_receives_proper_context_data(self):
         """Tests that article page returns proper data in context.
         """
-        response = self.client.get(self.article.get_absolute_url())
+        response = self.client.get(self.article_url)
         self.assertEqual(self.article, response.context["article"])
