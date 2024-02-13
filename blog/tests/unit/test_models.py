4b3809c98c90952da25ec33bc1361de0bb69eba4
BlackstarDamien <damianzylka@gmail.com>
2024-02-13 20:19:02 +0100

    Small refactoring of unit tests

--- blog/tests/unit/test_models.py
+++ blog/tests/unit/test_models.py
@@ -10,8 +10,7 @@ class TestArticle(TestCase):
             "title": "Test Article",
             "content": "Test Content"
         }
-        Article.objects.create(**self.data)
-        self.article = Article.objects.get(title=self.data["title"])
+        self.article = self.create_dummy_article(self.data)
     
     def test_should_create_new_article(self):
         self.assertEqual(self.article.title, self.data["title"])
@@ -25,8 +24,7 @@ class TestArticle(TestCase):
     
     def test_throws_error_when_slug_not_unique(self):
         with self.assertRaises(IntegrityError):
-            Article.objects.create(title="Test Article", 
-                                   content="New content")
+            self.create_dummy_article(self.data)
     
     def test_should_calculate_url_for_given_object(self):
         self.assertEqual(f"/articles/{self.article.slug}", 
@@ -36,3 +34,6 @@ class TestArticle(TestCase):
         current_date = datetime.now().strftime("%Y-%m-%d")
         expected_date = self.article.publish_date.strftime("%Y-%m-%d")
         self.assertEqual(expected_date, current_date)
+
+    def create_dummy_article(self, data):
+        return Article.objects.create(**data)
