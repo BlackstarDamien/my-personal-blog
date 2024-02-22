10e3b8c50aac73997f31bccb0eb61baf197dfaa8
BlackstarDamien <damianzylka@gmail.com>
2024-02-22 20:08:25 +0100

    Create about_me_page() view

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
-from blog.models import Article
+from blog.models import Article, AboutMe
 
 
 def main_page(request: HttpRequest) -> HttpResponse:
@@ -12,3 +12,8 @@ def article_page(request: HttpRequest, slug: str) -> HttpResponse:
     article = Article.objects.get(slug=slug)
     context = {"article": article}
     return render(request, 'article.html', context)
+
+def about_me_page(request: HttpRequest) -> HttpResponse:
+    about_me = AboutMe.objects.first()
+    context = {"about_me": about_me}
+    return render(request, 'about_me.html', context)
