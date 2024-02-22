f1ae1ca63ead5c93771fa3f01257b1a339f05a6b
BlackstarDamien <damianzylka@gmail.com>
2024-02-22 20:14:51 +0100

    Add e2e test test_clicking_on_logo_redirects_to_main_page() for About Me Page

--- blog/tests/e2e/test_about_me_page.py
+++ blog/tests/e2e/test_about_me_page.py
@@ -21,6 +21,17 @@ class TestAboutMePage(TestBase):
         self.when_click_link("About Me")
         self.then_i_can_see_about_me_page()
     
+    def test_clicking_on_logo_redirects_to_main_page(self):
+        """Tests that clicking on blog's name redirect user
+        to the main page.
+        """
+        self.given_an_about_me_page()
+        self.when_click_link("My Personal Blog")
+        self.then_i_am_on_the_page("main")
+    
+    def given_an_about_me_page(self):
+        self.browser.get(f"{self.live_server_url}/about-me")
+    
     def then_i_can_see_about_me_page(self):
         """Checks if About Me page is displayed properly.
         """
@@ -39,8 +50,6 @@ class TestAboutMePage(TestBase):
             "content": self.about_me["content"]
         }
         self.assertDictEqual(current_about_me, expected_about_me)
-    
-    # Write test that checks if you can nagivate to main paga via logo
     
     def create_dummy_about_me_page(self, data: Dict[str, str]):
         AboutMe.objects.create(**data)
