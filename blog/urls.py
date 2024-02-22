8cad7b42d781cc4a7914bc2f8546404dc8793e78
BlackstarDamien <damianzylka@gmail.com>
2024-02-22 20:09:15 +0100

    Include url to About Me Page

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from blog.views import main_page, article_page
+from blog.views import about_me_page, article_page, main_page
 
 urlpatterns = [
     path("", main_page, name="index"),
+    path("about-me", about_me_page, name="about_me_page"),
     path("articles/<slug:slug>", article_page, name="article_page")
 ]
