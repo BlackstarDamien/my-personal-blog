e4dbf4b2815441d9158d474ae1ea96097d08a852
BlackstarDamien <damianzylka@gmail.com>
2024-03-07 23:25:41 +0100

    Remove navigaton related tests

--- blog/tests/e2e/test_article_page.py
+++ blog/tests/e2e/test_article_page.py
@@ -8,8 +8,7 @@ class TestArticlePage(TestBase):
     def setUp(self) -> None:
         super().setUp()
         self.current_article = self.create_dummy_article(self.article)
-        self.create_dummy_about_me_page(self.about_me)
-        
+ 
     def test_displays_article_page(self):
         """Tests that article page is displayed correctly
         with following sections:
@@ -20,30 +19,6 @@ class TestArticlePage(TestBase):
         self.given_a_page("Main")
         self.when_click_link(self.article["title"])
         self.then_i_can_see_article_page(self.article["title"])
-    
-    def test_clicking_on_logo_redirects_to_main_page(self):
-        """Tests that clicking on blog's name redirect user
-        to the main page.
-        """
-        self.given_a_page("Article", self.current_article.slug)
-        self.when_click_link("My Personal Blog")
-        self.then_i_am_on_the_main_page()
-    
-    def test_clicking_on_posts_tab_redirects_to_main_page(self):
-        """Tests that clicking on 'Posts' tab in navbar redirect user
-        to the main page.
-        """
-        self.given_a_page("Article", self.current_article.slug)
-        self.when_click_link("Posts")
-        self.then_i_am_on_the_main_page()
-    
-    def test_clicking_on_about_me_tab_redirects_to_about_me_page(self):
-        """Tests that clicking on 'About Me' tab in navbar redirect user
-        to the About Me page.
-        """
-        self.given_a_page("Article", self.current_article.slug)
-        self.when_click_link("About Me")
-        self.then_i_can_see_about_me_page()
     
     def then_i_can_see_article_page(self, page_name: str):
         """Checks if article is displayed properly.
