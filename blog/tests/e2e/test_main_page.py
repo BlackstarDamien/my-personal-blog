f16a9a051211430c77b0df5769e48ab9485c0ec2
BlackstarDamien <damianzylka@gmail.com>
2024-03-25 20:24:14 +0100

    Modify e2e test suite for MainPage to use related page object

--- blog/tests/e2e/test_main_page.py
+++ blog/tests/e2e/test_main_page.py
@@ -1,28 +1,14 @@
-from selenium.webdriver.common.by import By
 from blog.tests.e2e.base import TestBase
-
+from .pages.main_page import MainPage
 
 class TestMainPage(TestBase):
     def test_displays_list_of_articles(self):
         """Tests that main page displays list of existing articles.
         """
         self.create_dummy_articles()
-        self.given_a_page("Main")
-        self.when_vist_this_page("My Personal Blog")
-        self.then_i_see_list_of_articles()
-    
-    def when_vist_this_page(self, page_name: str):
-        """Checks if user is on main page
+        self.browser.get(self.live_server_url)
+        
+        main_page = MainPage(self.browser)
+        articles = main_page.fetch_articles()
 
-        Parameters
-        ----------
-        page_name : str
-            Name of the page.
-        """
-        self.assertEqual(page_name, self.browser.title)
-    
-    def then_i_see_list_of_articles(self):
-        """Checks if there are any articles displayed on main page.
-        """
-        articles = self.browser.find_elements(By.CSS_SELECTOR, ".article-link")
         self.assertTrue(len(articles) > 0)
