673e4380bce37c3083de8fc2a3df5f7a4804b820
BlackstarDamien <damianzylka@gmail.com>
2024-04-03 11:07:08 +0200

    Create AdminLoginPage

---
+++ blog/tests/e2e/pages/admin_login_page.py
@@ -0,0 +1,39 @@
+from selenium.webdriver.common.by import By
+
+from .base_page import BasePage
+
+
+class AdminLoginPage(BasePage):
+    LOGIN_LOCATOR = (By.ID,'id_username')
+    PWD_LOCATOR = (By.ID,'id_password')
+    SUBMIT_BTN_LOCATOR = (By.XPATH, '//input[@value="Log in"]')
+
+    def navigate_to_admin_page(self):
+        """Moves user to admin page of given website.
+        """
+        admin_page_url = self.driver.current_url + "/admin"
+        self.driver.get(admin_page_url)
+
+    def login(self, username: str, password: str):
+        """Fill login form with given username
+        and password
+
+        Parameters
+        ----------
+        username : str
+            Provided username
+        password : str
+            Provided password
+        """
+        self.__enter_username(username)
+        self.__enter_password(password)
+        self.__submit_login_form()
+
+    def __enter_username(self, username: str):
+        self.find(self.LOGIN_LOCATOR)[0].send_keys(username)
+    
+    def __enter_password(self, password: str):
+        self.find(self.PWD_LOCATOR)[0].send_keys(password)
+    
+    def __submit_login_form(self):
+        self.find(self.SUBMIT_BTN_LOCATOR)[0].click()
