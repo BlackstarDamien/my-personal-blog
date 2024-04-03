820e94d2bd48966b7d6bbb38172369026dcfe3e1
BlackstarDamien <damianzylka@gmail.com>
2024-04-03 11:36:52 +0200

    Changes in MainPage: - Change the implementation of fetch_articles() - Add new method is_article_visible()

--- blog/tests/e2e/pages/main_page.py
+++ blog/tests/e2e/pages/main_page.py
@@ -3,16 +3,43 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
 
 from .base_page import BasePage
+from .article_page import ArticlePage
 
 class MainPage(BasePage):
     ARTICLES_LOCATOR = (By.CSS_SELECTOR, ".article-link")
     
-    def fetch_articles(self) -> List[WebElement]:
+    def fetch_articles(self) -> List[ArticlePage]:
         """Fetch and return the list of articles.
 
         Returns
         -------
-        List[WebElement]
+        List[ArticlePage]
             List of articles from main page.
         """
-        return self.find(self.ARTICLES_LOCATOR)
+        articles = []
+        for article_locator in self.find(self.ARTICLES_LOCATOR):
+            article_page = ArticlePage(self.driver)
+            article_page.navigate_to_article(article_locator.text)
+
+        return articles
+
+    def is_article_visible(self, title: str) -> bool:
+        """Checks if given article is visible on main page.
+
+        Parameters
+        ----------
+        title : str
+            Checked article's title
+
+        Returns
+        -------
+        bool
+            True if exists, False otherwise.
+        """
+        all_articles = self.fetch_articles()
+        for article in all_articles:
+            if article.get_title() == title:
+                return True
+        
+        return False
+    
