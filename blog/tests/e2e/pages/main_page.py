acdcbf9361bc4aee1c0c28c50478575a41f108cd
BlackstarDamien <damianzylka@gmail.com>
2024-04-03 12:05:31 +0200

    Remove unused import

--- blog/tests/e2e/pages/main_page.py
+++ blog/tests/e2e/pages/main_page.py
@@ -1,6 +1,5 @@
 from typing import List
 from selenium.webdriver.common.by import By
-from selenium.webdriver.remote.webelement import WebElement
 
 from .base_page import BasePage
 from .article_page import ArticlePage
