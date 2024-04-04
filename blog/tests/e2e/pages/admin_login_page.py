5858910215e0b26c897e22560309c201bbd2b2c2
BlackstarDamien <damianzylka@gmail.com>
2024-04-04 12:08:09 +0200

    Changes in AdminLoginPage: - navigate returns instances of AdminLoginPage - login() returns AdminPage

--- blog/tests/e2e/pages/admin_login_page.py
+++ blog/tests/e2e/pages/admin_login_page.py
@@ -1,6 +1,7 @@
 from selenium.webdriver.common.by import By
 
 from .base_page import BasePage
+from .admin_page import AdminPage
 
 
 class AdminLoginPage(BasePage):
@@ -8,15 +9,16 @@ class AdminLoginPage(BasePage):
     PWD_LOCATOR = (By.ID,'id_password')
     SUBMIT_BTN_LOCATOR = (By.XPATH, '//input[@value="Log in"]')
 
-    def navigate(self):
+    def navigate(self) -> "AdminLoginPage":
         """Moves user to admin page of given website.
         """
         admin_page_url = self.driver.current_url + "/admin"
         self.driver.get(admin_page_url)
+        return self
 
-    def login(self, username: str, password: str):
-        """Fill login form with given username
-        and password
+    def login(self, username: str, password: str) -> AdminPage:
+        """Fill login form with given username and password,
+        and redirects to Admin Page.
 
         Parameters
         ----------
@@ -24,10 +26,16 @@ class AdminLoginPage(BasePage):
             Provided username
         password : str
             Provided password
+        
+        Returns
+        -------
+        AdminPage
+            Admin page
         """
         self.__enter_username(username)
         self.__enter_password(password)
         self.__submit_login_form()
+        return AdminPage(self.driver)
 
     def __enter_username(self, username: str):
         self.find(self.LOGIN_LOCATOR)[0].send_keys(username)
