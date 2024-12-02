e41a29aaeb9a03af8db3a7cfb879283efd2122eb
BlackstarDamien <damianzylka@gmail.com>
2024-12-02 20:08:41 +0100

    Add attach_images() into AdminArticlePage

--- blog/tests/e2e/pages/admin_page.py
+++ blog/tests/e2e/pages/admin_page.py
@@ -24,10 +24,18 @@ class AdminArticlePage(AdminBasePage):
     ADD_LOCATOR = (By.XPATH, "//a[@href='/admin/blog/article/add/']")
     ARTICLES_LOCATOR = (By.LINK_TEXT, "Articles")
 
-    def add_new_article(self, article: dict):
+    def add_new_article(self, article: dict) -> "AdminArticlePage":
         self._click_on_add_article()
         self._enter_title(article["title"])
         self._enter_content(article["content"])
+        return self
+    
+    def attach_images(self, path_to_image: str) -> "AdminArticlePage":
+        file_input = self.find((By.CSS_SELECTOR, "input[type='file']"))
+        file_input.send_keys(path_to_image)
+        return self
+    
+    def save(self):
         self._click_save_button()
     
     def edit_article(self, title: str, changes: dict):
