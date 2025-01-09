534a33559f00661f24c5830663249788550bf8f1
BlackstarDamien <damianzylka@gmail.com>
2025-01-09 22:00:20 +0100

    Create ImageInline

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from blog.models import AboutMe, Article
+from blog.models import AboutMe, Article, Image
 
+class ImageInline(admin.TabularInline):
+    model = Image
+    extra = 1
 
 class ArticleAdmin(admin.ModelAdmin):
     fields = ["title", "content"]
+    inlines = [ImageInline]
 
 admin.site.register(AboutMe)
 admin.site.register(Article, ArticleAdmin)
