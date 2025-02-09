792cee186b81246c9699de0ed2231ba3dbaaec9d
BlackstarDamien <damianzylka@gmail.com>
2025-02-09 17:27:27 +0100

    Change implementation of DjangoMediaImageExtension

--- blog/extensions.py
+++ blog/extensions.py
@@ -1,25 +1,33 @@
 from markdown import Extension
 from markdown.treeprocessors import Treeprocessor
+from django.conf import settings
+from typing import Dict
 
 
 class DjangoMediaImageTreeProcessor(Treeprocessor):
     def run(self, root):
         """Extension logic"""
         images = root.iter("img")
+        img_url_map = self.config["img_url_map"]
         for img in images:
-            if img.get("src"):
-                img.set("src", "{{ MEDIA_URL }}/" + img.get("src"))
+            if img.get("src") and img.get("src") in img_url_map:
+                img_url = img_url_map[img.get("src")]
+                img.set("src", settings.MEDIA_URL + img_url)
 
 
 class DjangoMediaImageExtension(Extension):
     def __init__(self, **kwargs):
         """Initialize configuration"""
+        self.config = {
+            "img_url_map": [{}, "URLs for every defined image"]
+        }
         super(DjangoMediaImageExtension, self).__init__(**kwargs)
     
     def extendMarkdown(self, md):
         """Include DjangoMediaImageExtension into Markdown instance."""
         md.registerExtension(self)
         extension = DjangoMediaImageTreeProcessor(md)
+        extension.config = self.getConfigs()
         md.treeprocessors.register(
             extension,
             "django_media_image_pattern",
--- blog/tests/unit/test_extensions.py
+++ blog/tests/unit/test_extensions.py
@@ -1,16 +1,21 @@
 import markdown
 import unittest
 from blog.extensions import DjangoMediaImageExtension
+from django.conf import settings
 
 
 class TestDjangoMediaImage(unittest.TestCase):
     def test_img_tag_with_local_source(self):
         """Tests that path to local file can be parsed correctly."""
-        extension = DjangoMediaImageExtension()
+        img_url_map = {
+            "black-cat.jpg": "/images/black-cat.jpg"
+        }
+        extension = DjangoMediaImageExtension(img_url_map=img_url_map)
         md = markdown.Markdown(extensions=[extension])
 
-        text = """![](images/black-cat.jpg)"""
+        text = """![](black-cat.jpg)"""
         result = md.convert(text)
 
-        expected = '<p><img alt="" src="{{ MEDIA_URL }}/images/black-cat.jpg" /></p>'
+        image_path = settings.MEDIA_URL + "/images/black-cat.jpg"
+        expected = f'<p><img alt="" src="{image_path}" /></p>'
         self.assertEqual(result, expected)
