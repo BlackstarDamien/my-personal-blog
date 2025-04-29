5a3570b5f7774e7ded6e7f06bb8e3d2d3e56dfa7
BlackstarDamien <damianzylka@gmail.com>
2025-04-29 20:28:12 +0200

    refactor: split unit tests for models into separate test modules

--- blog/tests/unit/test_article.py
+++ blog/tests/unit/test_article.py
@@ -1,11 +1,8 @@
 from datetime import datetime
 from django.test import TestCase
 from django.db.utils import IntegrityError
-from blog.models import AboutMe
 from blog.tests.helpers import (
-    create_dummy_image,
-    create_dummy_article,
-    create_dummy_about_me
+    create_dummy_article
 )
 
 
@@ -56,51 +53,3 @@ class TestArticle(TestCase):
         current_date = datetime.now().strftime("%Y-%m-%d")
         expected_date = self.article.publish_date.strftime("%Y-%m-%d")
         self.assertEqual(expected_date, current_date)
-
-
-class TestAboutMe(TestCase):
-    def setUp(self) -> None:
-        self.data = {
-            "title": "About Me",
-            "content": "Something about me"
-        }
-        self.about_me = create_dummy_about_me(self.data)
-    
-    def test_should_create_new_about_me_instance(self):
-        """Checks if AboutMe object is initialized correctly.
-        """
-        self.assertEqual(self.about_me.title, self.data["title"])
-        self.assertEqual(self.about_me.content, self.data["content"])
-    
-    def test_save_not_create_new_instance(self):
-        """Ensures that there can be only one instance of AboutMe
-        created.
-        """
-        data = {
-            "title": "About Me",
-            "content": "Something new about me"
-        }
-        create_dummy_about_me(data)
-        about_me_count = AboutMe.objects.all().count()
-        about_me_updated = AboutMe.objects.first()
-
-        self.assertEqual(about_me_count, 1)
-        self.assertEqual(about_me_updated.content, data["content"])
-
-
-class TestImage(TestCase):
-    def setUp(self):
-        self.article = create_dummy_article(
-            {
-                "title": "Test Article",
-                "content": "Test Content"
-            }
-        )
-    
-    def test_should_create_new_instance(self):
-        """Checks if object model for Image is created properly.
-        """
-        test_image_name = "black-cat.jpg"
-        image = create_dummy_image(test_image_name)
-        self.assertEqual(image.name, test_image_name)
-        self.assertEqual(str(image.url).split("/")[-1], test_image_name)
