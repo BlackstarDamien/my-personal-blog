d5cc9dbb879b1d33134dca87f20dd5c9af7c7f96
BlackstarDamien <damianzylka@gmail.com>
2025-04-29 20:11:58 +0200

    refactor: move function that create dummy test data into separate module

---
+++ blog/tests/helpers/functions.py
@@ -0,0 +1,73 @@
+from io import BytesIO
+from PIL import Image as PILImage
+from blog.models import AboutMe, Article, Image
+from django.core.files.images import ImageFile
+
+
+def create_dummy_about_me(data: dict) -> AboutMe:
+    """Initialize instance of AboutMe based on dummy data.
+
+    Parameters
+    ----------
+    data : dict
+        Dummy data used to create AboutMe's object.
+
+    Returns
+    -------
+    AboutMe
+        Dummy AboutMe object.
+    """
+    return AboutMe.objects.create(**data)
+
+def create_dummy_article(data: dict) -> Article:
+    """Initialize instance of Article based on dummy data.
+
+    Parameters
+    ----------
+    data : dict
+        Dummy data used to create Article's object.
+
+    Returns
+    -------
+    Article
+        Dummy Article object.
+    """
+    return Article.objects.create(**data)
+   
+
+def create_test_image(file_name: str) -> Image:
+    """Initialize instance of Image model.
+
+    Parameters
+    ----------
+    file_name : str
+        Name of dummy Image file.
+
+    Returns
+    -------
+    Image
+        Dummy Image object.
+    """
+    test_image_content = __create_temp_img_content()
+    image_file = ImageFile(
+            file=test_image_content,
+            name=file_name
+    )
+
+    article_image = Image(
+        name=file_name,
+        url=image_file,
+    )
+
+    return article_image
+
+def __create_temp_img_content() -> BytesIO:
+    temp_img = BytesIO()
+    image = PILImage.new(
+        mode="RGB",
+        size=(200, 200),
+        color=(255, 0, 0, 0)
+    )
+    image.save(temp_img, "jpeg")
+    temp_img.seek(0)
+    return temp_img
