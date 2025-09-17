f586200970234818fa5e0bee237018b400196fd5
blackstardamien <damianzylka@gmail.com>
2025-09-17 20:07:29 +0000

    fix: streamline live server URL setup in test base class

--- blog/tests/e2e/base.py
+++ blog/tests/e2e/base.py
@@ -21,14 +21,11 @@ class TestBase(LiveServerTestCase):
         self.browser = self.browser_container.get_driver()
         self.browser.implicitly_wait(3)
 
-        # #TODO: Remove it
-        # print(f"DEBUG: Before {self.live_server_url}")
-        
-        # server_host = os.environ.get("TEST_HOST", "host.docker.internal")
-        # self.live_server_url = f'http://{server_host}:{self.port}/'
+        server_host = os.environ.get("TEST_HOST", "host.docker.internal")
+        self.live_server_url = f'http://{server_host}:{self.port}/'
         
         # #TODO: Remove it
-        print(f"DEBUG: After {self.live_server_url}")
+        print(f"DEBUG: {self.live_server_url}")
         
         self.browser.get(self.live_server_url)
 
