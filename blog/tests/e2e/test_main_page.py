0c0de544abdebaacd63a31c40571b097d5639594
blackstardamien <damianzylka@gmail.com>
2025-09-23 20:35:59 +0000

    fix: make TestMainPage work

--- blog/tests/e2e/test_main_page.py
+++ blog/tests/e2e/test_main_page.py
@@ -1,11 +1,13 @@
 from blog.tests.e2e.base import TestBase
 from .pages.main_page import MainPage
-from blog.tests.helpers import create_dummy_articles
+from blog.tests.helpers import create_dummy_articles, create_dummy_article
 
 class TestMainPage(TestBase):
     def setUp(self):
         super().setUp()
-        self.articles = create_dummy_articles(self.test_articles)
+        # self.articles = create_dummy_articles(self.test_articles)
+        for article in self.test_articles:
+            create_dummy_article(article)
         
     def test_displays_list_of_articles(self):
         """Tests that main page displays list of existing articles.
