fd5333aec477120f12e5541e1294f74daf0cca95
blackstardamien <damianzylka@gmail.com>
2025-09-23 19:35:36 +0000

    refactor: move create_dummy_articles() to test method again

--- blog/tests/e2e/test_main_page.py
+++ blog/tests/e2e/test_main_page.py
@@ -5,11 +5,11 @@ from blog.tests.helpers import create_dummy_articles
 class TestMainPage(TestBase):
     def setUp(self):
         super().setUp()
-        create_dummy_articles(self.test_articles)
 
     def test_displays_list_of_articles(self):
         """Tests that main page displays list of existing articles.
         """
+        create_dummy_articles(self.test_articles)
         main_page = MainPage(self.browser).navigate()
         articles = main_page.fetch_articles()
 
