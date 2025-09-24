5299a0aba4b3d9e020d8f99b0c68cf8af4b59c66
blackstardamien <damianzylka@gmail.com>
2025-09-24 17:47:46 +0000

    fix: fix tests

--- blog/tests/e2e/test_main_page.py
+++ blog/tests/e2e/test_main_page.py
@@ -1,18 +1,15 @@
 from blog.tests.e2e.base import TestBase
 from .pages.main_page import MainPage
-from blog.tests.helpers import create_dummy_articles, create_dummy_article
+from blog.tests.helpers import create_dummy_articles
 
 class TestMainPage(TestBase):
     def setUp(self):
         super().setUp()
-        # self.articles = create_dummy_articles(self.test_articles)
-        for article in self.test_articles:
-            create_dummy_article(article)
         
     def test_displays_list_of_articles(self):
         """Tests that main page displays list of existing articles.
         """
-        # create_dummy_articles(self.test_articles)
+        create_dummy_articles(self.test_articles)
         main_page = MainPage(self.browser).navigate()
         articles = main_page.fetch_articles()
 
