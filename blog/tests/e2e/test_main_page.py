ad355518670a59a64a4bf46760776560538d13bb
blackstardamien <damianzylka@gmail.com>
2025-09-24 18:04:58 +0000

    fix: add real test database

--- blog/tests/e2e/test_main_page.py
+++ blog/tests/e2e/test_main_page.py
@@ -1,6 +1,6 @@
 from blog.tests.e2e.base import TestBase
 from .pages.main_page import MainPage
-from blog.tests.helpers import create_dummy_articles
+from blog.tests.helpers import create_dummy_articles, create_dummy_article
 
 class TestMainPage(TestBase):
     fixtures = ["articles.json"]
@@ -11,7 +11,12 @@ class TestMainPage(TestBase):
     def test_displays_list_of_articles(self):
         """Tests that main page displays list of existing articles.
         """
-        create_dummy_articles(self.test_articles)
+        # create_dummy_articles(self.test_articles)
+
+        for article in self.test_articles:
+            a = create_dummy_article(article)
+            a.save()
+            
         main_page = MainPage(self.browser).navigate()
         articles = main_page.fetch_articles()
 
