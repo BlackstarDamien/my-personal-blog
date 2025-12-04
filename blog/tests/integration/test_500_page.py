83508264f26d3ab55005f0ac9461913e3e9a1138
BlackstarDamien <damianzylka@gmail.com>
2025-12-04 19:53:34 +0000

    feat: add server error page and integration tests for 500 error handling

---
+++ blog/tests/integration/test_500_page.py
@@ -0,0 +1,43 @@
+from django.http import HttpRequest, HttpResponse
+from django.test import TestCase, Client
+from django.urls import path, clear_url_caches
+from blog.views import server_error_view
+from blog.urls import urlpatterns
+
+
+def trigger_500_error(request: HttpRequest) -> HttpResponse:
+    raise Exception("Intentional server error for testing purposes")
+
+class Test500Page(TestCase):
+    """Integration tests for 500 error page."""
+
+    def setUp(self):
+        self.client = Client(raise_request_exception=False)
+        urlpatterns.append(
+            path('trigger-500-error', trigger_500_error, name='trigger_500_error')
+        )
+        clear_url_caches()
+
+    def test_500_page_returns_correct_status_code(self):
+        """Test 500 error page view returns correct status code."""
+        response = server_error_view(self.client.get('/trigger-500-error'))
+        self.assertEqual(response.status_code, 500)
+
+    def test_500_page_uses_correct_template(self):
+        """Test that 500 error page uses proper template."""
+        response = self.client.get('/trigger-500-error')
+        self.assertTemplateUsed(response, '500.html')
+
+    def test_500_page_contains_error_message(self):
+        """Test that 500 error page contains appropriate error message."""
+        response = self.client.get('/trigger-500-error')
+        self.assertContains(
+            response, 
+            "Something went wrong on our end", 
+            status_code=500
+        )
+
+    def test_500_page_content_type(self):
+        """Test that 500 error page returns HTML content type."""
+        response = server_error_view(self.client.get('/trigger-500-error'))
+        self.assertEqual(response['Content-Type'], 'text/html; charset=utf-8')
