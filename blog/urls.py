10ae49d4cee950da8997e086164f5a06f4302f97
BlackstarDamien <damianzylka@gmail.com>
2025-12-11 18:49:11 +0000

    feat: remove custom error handlers from urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-from django.conf.urls import handler404, handler500
 
 from blog.views import about_me_page, article_page, main_page
 
@@ -8,6 +7,3 @@ urlpatterns = [
     path("about-me", about_me_page, name="about_me_page"),
     path("articles/<slug:slug>", article_page, name="article_page")
 ]
-
-handler404 = "blog.views.page_not_found_view"
-handler500 = "blog.views.server_error_view"
